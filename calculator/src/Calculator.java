f05525f05df3fba656516b1a47c9f5f36691d682
Stephy-Duran <stephany.duran@globant.com>
2024-08-09 14:13:06 -0500

    Add sum and subtract methods to Calculator class

--- calculator/src/Calculator.java
+++ calculator/src/Calculator.java
@@ -1,2 +1,10 @@
 public class Calculator {
+
+    public double sum(double num1, double num2){
+        return num1+num2;
+    }
+    public double subtract(double num1, double num2){
+        return num1-num2;
+    }
+
 }
